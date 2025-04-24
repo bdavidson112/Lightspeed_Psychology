797901ee634a0304754a5a0b5dee5060adaddfbd
bdavidson112 <bdavidson112@gmail.com>
2025-04-24 15:38:41 -0700

    adjusted Dir

--- vite.config.ts
+++ vite.config.ts
@@ -26,9 +26,12 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
+  
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
+  publicDir: path.resolve(import.meta.dirname, "public"),
+
   assetsInclude: ["**/*.JPG", "**/*.jpg"],
 });
